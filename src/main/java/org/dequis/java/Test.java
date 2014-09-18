73000e1af4a157b80fadb71e0905fcad3780a58b
dequis <dx@dxzone.com.ar>
2014-09-18 07:49:47 -0300

    PlayerLoginEvent, test for event.getPlayer().isOnline()

--- src/main/java/org/dequis/java/Test.java
+++ src/main/java/org/dequis/java/Test.java
@@ -5,6 +5,7 @@ import org.bukkit.command.*;
 import org.bukkit.entity.*;
 import org.bukkit.event.*;
 import org.bukkit.event.block.*;
+import org.bukkit.event.player.*;
 import org.bukkit.plugin.java.*;
 import org.bukkit.plugin.*;
 import org.bukkit.inventory.*;
@@ -17,15 +18,11 @@ public class Test extends JavaPlugin implements Listener, CommandExecutor {
     public void onEnable() {
         PluginManager pm = this.getServer().getPluginManager();
         pm.registerEvents(this, this);
-        getCommand("command").setExecutor(this);
     }
 
-    @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (command.getName().equalsIgnoreCase("command")) {
-            getLogger().info("command used");
-            return true;
-        }
-        return false;
+    @EventHandler
+    public void onPlayerLogin(PlayerLoginEvent event) {
+        getLogger().info("onPlayerLogin: player.isOnline() == " + event.getPlayer().isOnline());
+
     }
 }
