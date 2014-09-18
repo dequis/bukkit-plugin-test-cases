4502e409bfd26be352f1995331d260a2c4d9b871
dequis <dx@dxzone.com.ar>
2014-09-18 05:25:11 -0300

    initial test plugin skeleton

---
+++ src/main/java/org/dequis/java/Test.java
@@ -0,0 +1,19 @@
+package org.dequis.java;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.*;
+import org.bukkit.entity.*;
+import org.bukkit.event.*;
+import org.bukkit.plugin.java.*;
+import org.bukkit.plugin.*;
+import org.bukkit.inventory.*;
+import java.util.*;
+import java.util.logging.*;
+
+
+public class Test extends JavaPlugin implements Listener, CommandExecutor {
+
+    public void onEnable() {
+    }
+
+}
